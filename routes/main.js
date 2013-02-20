d2f167ce43f62d5d20eb4a025b08df98e88bfdab
Michael <mbg@outlook.com>
2013-02-20 14:31:58 +0000

    First commit

---
+++ routes/main.js
@@ -0,0 +1,24 @@
+var FlickrAPI = require('../flickrnode/lib/flickr').FlickrAPI
+, sys = require('sys')
+, flickr = new FlickrAPI('94cdcabe350621cb1d973fda25197f21');
+
+exports.home = function(req, res){
+  res.render('home', {title: "Geoflickr"});
+};
+
+exports.test = function(req, res){
+
+	function searchObject(obj) {
+		this.lat = obj.lat;
+		this.lon = obj.lon;
+		this.license = obj.licenses;
+		this.min_date_upload = 946706400;
+		this.tags = obj.tag;
+	}
+
+	var sO = new searchObject(req.body);
+	flickr.photos.search(sO, function(error, results) {
+		res.json(results);
+	});
+}
+
