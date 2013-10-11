8698d6722c3c5d920da6405aba47c1e3fe9abba7
Michael <mbg@outlook.com>
2013-10-11 20:21:03 +0100

    Update to coords

--- public/javascript/geoflickr.js
+++ public/javascript/geoflickr.js
@@ -1,6 +1,6 @@
 function create_map() {
     var mapOptions = {
-        center: new google.maps.LatLng(51.50678771873268, -0.1271748905517142),
+        center: new google.maps.LatLng(25.48295117535531, 30.234375),
         zoom: 2,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         panControl: false,
