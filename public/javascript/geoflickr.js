dd5c5b7c3d621ae4a267f737e21dbd0dfceaf077
Michael <mbg@outlook.com>
2013-10-11 14:49:13 +0100

    Update geoflickr.js

--- public/javascript/geoflickr.js
+++ public/javascript/geoflickr.js
@@ -205,8 +205,8 @@ function processFlickrData(photos) {
 
             var thumbnail = "http://farm" + farmId + ".staticflickr.com/" + serverId + "/" + photoId + "_" + photoSecret + "_t.jpg"
             var mainurl = "http://farm" + farmId + ".staticflickr.com/" + serverId + "/" + photoId + "_" + photoSecret + ".jpg"
-            var flickrurl = "http://flickr.com/photo.gne?=" + photoId + "/"
-            $('#photos').append("<span>" + "<a href=" + mainurl + ">" + "<img src=" + thumbnail + "></a></span>" + "<div class=/"hiddenflickrlink/">" + "<a href=" + flickrurl + ">" + "Photo on Flickr</a></div>");
+           
+            $('#photos').append("<span>" + "<a href=" + mainurl + ">" + "<img src=" + thumbnail + "></a></span>" + "<div class=/"hiddenflickrlink/">");
 
         };
     }
